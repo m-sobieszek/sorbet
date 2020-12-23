ca0fdbdba77187ae3e77b7f13c39765174243425
Nathan Froyd <froydnj@gmail.com>
2020-12-23 17:08:48 -0500

    first cut at generating defined foreign key methods

--- ast/Helpers.h
+++ ast/Helpers.h
@@ -50,6 +50,12 @@ public:
         return Send(loc, std::move(recv), fun, 0, std::move(nargs), {}, std::move(blk));
     }
 
+    static TreePtr Send1Block(core::LocOffsets loc, TreePtr recv, core::NameRef fun, TreePtr arg1, TreePtr blk) {
+        Send::ARGS_store nargs;
+        nargs.emplace_back(std::move(arg1));
+        return Send(loc, std::move(recv), fun, 1, std::move(nargs), {}, std::move(blk));
+    }
+
     static TreePtr Send1(core::LocOffsets loc, TreePtr recv, core::NameRef fun, TreePtr arg1) {
         return Send(loc, std::move(recv), fun, 1, SendArgs(std::move(arg1)));
     }
@@ -61,6 +67,17 @@ public:
     static TreePtr Send3(core::LocOffsets loc, TreePtr recv, core::NameRef fun, TreePtr arg1, TreePtr arg2,
                          TreePtr arg3) {
         return Send(loc, std::move(recv), fun, 3, SendArgs(std::move(arg1), std::move(arg2), std::move(arg3)));
+    }
+
+    static TreePtr Send5(core::LocOffsets loc, TreePtr recv, core::NameRef fun, TreePtr arg1, TreePtr arg2,
+                         TreePtr arg3, TreePtr arg4, TreePtr arg5) {
+        Send::ARGS_store nargs;
+        nargs.emplace_back(std::move(arg1));
+        nargs.emplace_back(std::move(arg2));
+        nargs.emplace_back(std::move(arg3));
+        nargs.emplace_back(std::move(arg4));
+        nargs.emplace_back(std::move(arg5));
+        return Send(loc, std::move(recv), fun, 5, std::move(nargs));
     }
 
     static TreePtr Literal(core::LocOffsets loc, const core::TypePtr &tpe) {
